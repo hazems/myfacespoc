8448dc859be8c7da6c79d76bd7286f97851c0eac
Hazem.saleh <Hazem.saleh@cfe01ffa-9589-11dd-8950-3d966a63a0de>
2010-01-09 04:06:58 +0000

    The favorite places is a NetBeans application that uses: JSF 2.0 CDI EJB 3.1 JPA 2.0 GMaps4JSF 1.1.3

---
+++ favoritePlaces/src/java/code/google/com/favoritePlaces/persistence/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package code.google.com.favoritePlaces.persistence.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
---
+++ favoritePlaces/src/java/code/google/com/favoritePlaces/persistence/exceptions/RollbackFailureException.java
@@ -0,0 +1,10 @@
+package code.google.com.favoritePlaces.persistence.exceptions;
+
+public class RollbackFailureException extends Exception {
+    public RollbackFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public RollbackFailureException(String message) {
+        super(message);
+    }
+}
