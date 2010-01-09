a7664fc85e0c948f90126c6d8f79fe10569e6c49
Hazem.saleh <Hazem.saleh@cfe01ffa-9589-11dd-8950-3d966a63a0de>
2010-01-09 05:09:00 +0000

    Releasing the example.

---
+++ favoritePlaces/src/java/code/google/com/favoritePlaces/persistence/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package code.google.com.favoritePlaces.persistence;
+
+/**
+ *
+ * @author hazems
+ */
+public class NewClass {
+
+}
